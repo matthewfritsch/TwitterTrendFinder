849dc8c49a49af0167f9383fb4610ef3167111ee
Matthew Fritsch <matthewmfritsch@gmail.com>
2020-08-13 01:45:52 -0700

    pageReader will now crawl on a provided date

--- pageReader.py
+++ pageReader.py
@@ -1,30 +1,30 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-f = open('website.txt', 'r')
-#website = 'trend/' + f.readline() + "2020-08-12.html"
-website = f.readline()
-f.close()
+class pageReader:
 
-driver = webdriver.Chrome()
-driver.get(website)
+    def crawl(self, site):
 
-assert "404 NOT FOUND" not in driver.page_source
+        driver = webdriver.Chrome()
+        driver.get(site)
 
-driver.maximize_window()
-button = driver.find_element_by_link_text("More")
-button.click()
-topelems = driver.find_elements_by_css_selector("ol > li > a")
-for i in range(len(topelems)):
-    if topelems[i].text == "":
-        break
-topelems = topelems[0:i]
+        if "404 NOT FOUND"  in driver.page_source:
+            return "No values."
 
-results = '\n'.join([b.text for b in topelems])
+        driver.maximize_window()
+        button = driver.find_element_by_link_text("More")
+        button.click()
+        topelems = driver.find_elements_by_css_selector("ol > li > a")
+        for i in range(len(topelems)):
+            if topelems[i].text == "":
+                break
+        topelems = topelems[0:i]
 
-for e in topelems:
-    print(e.text)
-botelems = driver.find_elements_by_xpath("//ol[@id='readmore1']/li/a")
+        results = '\n'.join([b.text for b in topelems])
 
-results += '\n'.join([b.text for b in botelems])
-print(results)
+        for e in topelems:
+            print(e.text)
+        botelems = driver.find_elements_by_xpath("//ol[@id='readmore1']/li/a")
+
+        results += '\n'.join([b.text for b in botelems])
+        return results
