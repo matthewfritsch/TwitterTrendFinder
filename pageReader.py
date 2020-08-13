a55d43f5765608c447dbc4ce35f3ade16f56ba68
Matthew Fritsch <matthewmfritsch@gmail.com>
2020-08-13 01:24:42 -0700

    pageReader will successfully find all the trends and print them out for a provided date

--- pageReader.py
+++ pageReader.py
@@ -2,13 +2,15 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 f = open('website.txt', 'r')
-website = f.readline() + "2020-08-12.html"
+#website = 'trend/' + f.readline() + "2020-08-12.html"
+website = f.readline()
 f.close()
-
-results = ''
 
 driver = webdriver.Chrome()
 driver.get(website)
+
+assert "404 NOT FOUND" not in driver.page_source
+
 driver.maximize_window()
 button = driver.find_element_by_link_text("More")
 button.click()
@@ -18,11 +20,11 @@ for i in range(len(topelems)):
         break
 topelems = topelems[0:i]
 
-results = results + e for e in topelems
+results = '\n'.join([b.text for b in topelems])
 
 for e in topelems:
     print(e.text)
 botelems = driver.find_elements_by_xpath("//ol[@id='readmore1']/li/a")
-print(len(botelems))
-for b in botelems:
-    print(b.text)
+
+results += '\n'.join([b.text for b in botelems])
+print(results)
