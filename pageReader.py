59f0921a447f8eaa5ed264def9409c9803c1e5cd
Matthew Fritsch <matthewmfritsch@gmail.com>
2020-08-13 17:16:17 -0700

    pageReader now only opens a single driver, and at a slow pace to prevent spam

--- pageReader.py
+++ pageReader.py
@@ -1,20 +1,22 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from time import sleep
 
 class pageReader:
 
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+
     def crawl(self, site):
+        self.driver.get(site)
 
-        driver = webdriver.Chrome()
-        driver.get(site)
-
-        if "404 NOT FOUND"  in driver.page_source:
+        if "404 NOT FOUND"  in self.driver.page_source:
             return "No values."
 
-        driver.maximize_window()
-        button = driver.find_element_by_link_text("More")
+        self.driver.maximize_window()
+        button = self.driver.find_element_by_link_text("More")
         button.click()
-        topelems = driver.find_elements_by_css_selector("ol > li > a")
+        topelems = self.driver.find_elements_by_css_selector("ol > li > a")
         for i in range(len(topelems)):
             if topelems[i].text == "":
                 break
@@ -24,7 +26,8 @@ class pageReader:
 
         for e in topelems:
             print(e.text)
-        botelems = driver.find_elements_by_xpath("//ol[@id='readmore1']/li/a")
+        botelems = self.driver.find_elements_by_xpath("//ol[@id='readmore1']/li/a")
 
         results += '\n'.join([b.text for b in botelems])
+        
         return results
