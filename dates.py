f752813c75677ed904aded778c76c7f974f6bf14
Matthew Fritsch <matthewmfritsch@gmail.com>
2020-08-13 17:15:47 -0700

    dates now refers to all dates between 2016-02-02 and 2020-08-13

--- dates.py
+++ dates.py
@@ -1,18 +1,23 @@
 from datetime import date
-dates = []
 class dates:
+    allDates = []
     def __init__(self):
+        currYear = False
         for i in range(5):
             year = 2016 + i
-            for j in range(12):
+            currYear = year == 2020
+            for j in range(1, 13 if not currYear else 1+int(str(date.today())[5:7])):
+                if year == 2016 and j == 1:
+                    continue
                 month = j
-                for k in range(31):
+                dayCap = 31
+                for k in range(31 if not currYear else 1+int(str(date.today())[8:])):
                     day = k
                     d = 0
                     try:
                         d = date(year, month, day)
                     except ValueError:
                         continue
-    
+                    self.allDates.append(str(d))
     def getDates(self):
-        return self.dates
+        return self.allDates
