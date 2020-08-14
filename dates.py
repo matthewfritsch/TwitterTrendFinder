8cecb07a1d9dcb7c903cfab21e28c655d736f446
Matthew Fritsch <matthewmfritsch@gmail.com>
2020-08-13 20:43:48 -0700

    In case the program ends, a file now stores incomplete dates

--- dates.py
+++ dates.py
@@ -1,23 +1,48 @@
+from os import path
 from datetime import date
+from random import randint
 class dates:
-    allDates = []
+    
     def __init__(self):
-        currYear = False
-        for i in range(5):
-            year = 2016 + i
-            currYear = year == 2020
-            for j in range(1, 13 if not currYear else 1+int(str(date.today())[5:7])):
-                if year == 2016 and j == 1:
-                    continue
-                month = j
-                dayCap = 31
-                for k in range(31 if not currYear else 1+int(str(date.today())[8:])):
-                    day = k
-                    d = 0
-                    try:
-                        d = date(year, month, day)
-                    except ValueError:
+        self.lines = 0
+        if not path.exists('datesToFind.txt'):
+            f = open('datesToFind.txt', 'a')
+            for i in range(5):
+                year = 2016 + i
+                currYear = year == int(str(date.today())[:4])
+                for j in range(1, 13 if not currYear else 1+int(str(date.today())[5:7])):
+                    if year == 2016 and j == 1:
                         continue
-                    self.allDates.append(str(d))
-    def getDates(self):
-        return self.allDates
+                    month = j
+                    dayCap = 31
+                    for k in range(31 if not currYear else 1+int(str(date.today())[8:])):
+                        day = k
+                        d = 0
+                        try:
+                            d = date(year, month, day)
+                        except ValueError:
+                            continue
+                        f.write(str(d) + '\n')
+                        self.lines += 1
+            f.close()
+        else:
+            self.lines = len(open('datesToFind.txt', 'r').readlines( ))
+            print(self.lines)
+
+    def getRandomDate(self):
+        if self.lines == 0:
+            return None
+        date = ''
+        allelse = ''
+        with open('datesToFind.txt', 'r') as f:
+            allelse = f.readlines()
+        with open('datesToFind.txt', 'w') as f:
+            num = randint(1, self.lines)
+            self.lines -= 1
+            for line in allelse:
+                num-=1
+                if not (num == 0):
+                   f.write(line)
+                else:
+                    date = line.strip('\n')
+        return date
