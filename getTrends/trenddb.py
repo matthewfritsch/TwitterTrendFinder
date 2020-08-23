29df406a5aab7a0d0b7e38e3bc159c431e4e3928
Matthew Fritsch <matthewmfritsch@gmail.com>
2020-08-22 21:46:24 -0700

    changed database folder location

--- getTrends/trenddb.py
+++ getTrends/trenddb.py
@@ -3,21 +3,21 @@ from os import path
 from datetime import date
 
 class trenddb:
+    
+    
     def __init__(self):
-        self.db = DAL('sqlite://storage.db', folder=path.join("./database"))
+        self.db = DAL('sqlite://storage.db', folder=path.join("../database"))
         self.db.define_table('trenddates',
             Field('trend', 'text'),
             Field('date', 'date')
         )
         self.db.commit()
     
+
     def add(self, trends, trenddate):
-        
         if trends is None:
             return
-        
         f = open('runningTrends.txt', 'a')
-
         d = date(int(trenddate[:4]), int(trenddate[5:7]), int(trenddate[8:]))
         for t in trends:
             self.db.trenddates.insert(trend=t, date=d)
@@ -25,9 +25,8 @@ class trenddb:
             f.write("\n")
         f.close()
 
+
     def addParsed(self, trends, trenddate):
-
         d = date(int(trenddate[:4]), int(trenddate[5:7]), int(trenddate[8:]))
-
         for t in trends:
             self.db.trenddates.insert(trend=t, date=d)
