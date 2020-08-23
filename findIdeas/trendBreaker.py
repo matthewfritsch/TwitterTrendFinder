1404ebb11676481a8a0242037b0071f86dac68a6
Matthew Fritsch <matthewmfritsch@gmail.com>
2020-08-22 22:35:54 -0700

    changed plan for trendBreaker

--- findIdeas/trendBreaker.py
+++ findIdeas/trendBreaker.py
@@ -1,4 +1,21 @@
-#get db entries
-#parse every entry by word
-#add word to list AND to new db if word doesn't exist in list already
 
+#if entry not entirely capitals and entry not entirely lowers
+#   split by space
+#   split by cap
+#   split by pre-cap
+#   careful of numbers...
+#else
+#   forever:
+#       find largest 3+ letter word in entry
+#       if found
+#           put word into list and remove from entry
+#           try again
+#       else
+#           break
+#with list of words
+#for word in words
+#   for entry in db
+#       if word in entry
+#           if word not in newdb
+#               add word to newdb
+#           add entry to list of words
