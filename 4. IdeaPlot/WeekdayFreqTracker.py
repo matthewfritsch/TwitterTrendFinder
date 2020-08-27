1d78ba0d0276c277c103c26aa9c76d9dacb3b3f0
Matthew Fritsch <matthewmfritsch@gmail.com>
2020-08-26 20:52:17 -0700

    WeekdayFreqTracker manages the WeekdayFreqs from the database and provides a list back to weekdayTrends

---
+++ 4. IdeaPlot/WeekdayFreqTracker.py
@@ -0,0 +1,48 @@
+from dbgetter import dbgetter
+from WeekdayFreq import WeekdayFreq
+from string import punctuation
+class WeekdayFreqTracker:
+    def __init__(self):
+        self.weekdayFreqs = []
+        self.allDates = 0
+    
+    def remove_punc(self, s):
+        i = 0
+        maxIndex = len(s)
+        while i < maxIndex:
+            let = s[i]
+            if let in punctuation:
+                let.replace(let, '')
+                maxIndex = len(s)
+            i+=1
+        return s
+
+    def addWDF(self, trend, date):
+        for wdf in self.weekdayFreqs:
+            if wdf.trend == trend:
+                wdf.addDate(date)
+                return
+        self.weekdayFreqs.append(WeekdayFreq(trend, date))
+        
+
+    def get_weekday_freqs(self):
+        mydb = dbgetter('trends')
+        daysOfWeek = ['mon', 'tues', 'wednes', 'thurs', 'fri', 'satur', 'sun']
+
+        allTrends = [str(t) for t in mydb.getInfo('trend')]
+        allDates = [str(d) for d in mydb.getInfo('date')]
+        self.allDates = [] + allDates
+        
+        for i in range(len(allTrends)):
+            trend = self.remove_punc(allTrends[i].lower())
+            date = allDates[i]
+            if any(day+'day' in trend for day in daysOfWeek):
+                for day in daysOfWeek:
+                    if day+'day' in trend:
+                        self.addWDF(trend, date)
+        mydb.close()
+        return self.weekdayFreqs
+
+
+    def getAllDates(self):
+        return self.allDates
