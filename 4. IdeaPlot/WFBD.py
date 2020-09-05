297ffa1aebfd2ca47c71cc49c348e9c0d977b273
Matthew Fritsch <matthewmfritsch@gmail.com>
2020-09-05 00:45:19 -0700

    added functions to WFBD to make it easier to acquire data and add dates

--- 4. IdeaPlot/WFBD.py
+++ 4. IdeaPlot/WFBD.py
@@ -1,3 +1,4 @@
+#import time
 class WFBD:
     def __init__(self, word, freq):
         self.word = word
@@ -11,5 +12,27 @@ class WFBD:
         else:
             self.dates[d] = 1
     
+    def addDates(self, dates):
+        if dates is None:
+            return
+        for date in dates:
+            self.addDate(date)
+
     def getDatesSorted(self):
         return sorted(self.dates)
+
+    def getFreqOnDate(self, date):
+        if date in self.dates:
+            return self.dates[date]
+        else:
+            return 0
+
+    def getWord(self):
+        return self.word
+
+    def __str__(self):
+        toRet = self.word + ':'
+        for date, freq in self.dates.items():
+            toRet += "\n" + str(date) + ":" + str(freq)
+        #time.sleep(0.2)
+        return toRet
